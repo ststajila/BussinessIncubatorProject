1ada7d05ecb766a76f750aa62efd6cd54a7a6fbd
Stanislav <stazilastas@gmail.com>
2023-10-17 08:36:55 -0500

    Initial Commit

---
+++ Orderly/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Orderly
+//
+//  Created by STANISLAV STAJILA on 10/17/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
