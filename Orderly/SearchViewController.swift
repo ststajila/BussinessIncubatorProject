32306addc53af1a5da1489703f4b095713c5a819
Stanislav <stazilastas@gmail.com>
2023-11-03 09:35:23 -0500

    Home screen and a Map

---
+++ Orderly/SearchViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SearchViewController.swift
+//  Orderly
+//
+//  Created by STANISLAV STAJILA on 10/17/23.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
--- Orderly/ViewController.swift
+++ Orderly/ViewController.swift
@@ -12,8 +12,26 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.title = "Home"
+
     }
 
-
+    
+    @IBAction func goToList(_ sender: Any) {
+    performSegue(withIdentifier: "list", sender: self)
+    }
+    
+    @IBAction func goToCart(_ sender: Any) {
+    performSegue(withIdentifier: "cart", sender: self)
+    }
+    
+    
+    @IBAction func goToSearch(_ sender: Any) {
+        performSegue(withIdentifier: "search", sender: self)
+    }
+    
+    @IBAction func goToQr(_ sender: Any) {
+        performSegue(withIdentifier: "qr", sender: self)
+    }
 }
 
