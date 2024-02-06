46eaf4c78885ded063e7eecdb8488245f5d14a58
Stanislav <stazilastas@gmail.com>
2024-02-06 09:49:38 -0600

    Map and a picker

--- Orderly/StoreViewController.swift
+++ Orderly/StoreViewController.swift
@@ -8,36 +8,59 @@
 import UIKit
 import MapKit
 
-class StoreViewController: UIViewController, CLLocationManagerDelegate {
+class StoreViewController: UIViewController, CLLocationManagerDelegate, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    @IBOutlet weak var storePicker: UIPickerView!
+    var storeNames = ["Walmart", "Aldi", "JewelOsco"]
+    var pickedStore = ""
+    
     @IBOutlet weak var mapViewOutlet: MKMapView!
     let locationManager = CLLocationManager()
-    let currentLocation = CLLocation()
+    var currentLocation = CLLocation(latitude: 42.2411, longitude: -88.316978)
+    var stores: [MKMapItem] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        storePicker.delegate = self
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.startUpdatingLocation()
         mapViewOutlet.showsUserLocation = true
-        
-        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
-        let center = locationManager.location!.coordinate
-        let region = MKCoordinateRegion(center: center, span: span)
-        
-        mapViewOutlet.setRegion(region, animated: true)
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        currentLocation = locations[0]
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func search(value: String){
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = "Walmart"
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        request.region = MKCoordinateRegion(center: currentLocation.coordinate, span: span)
+        
+        let search = MKLocalSearch(request: request)
+        
+        search.start { (response, error) in
+            guard let response = response
+                    else{
+                        return
+                    }
+            for mapItem in response.mapItems{
+                self.stores.append(mapItem)
+                let annotation = MKPointAnnotation()
+                annotation.coordinate = mapItem.placemark.coordinate
+                annotation.title = mapItem.name
+                self.mapViewOutlet.addAnnotation(annotation)
+            }
+        }
     }
-    */
-
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return storeNames.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        <#code#>
+    }
+    
 }
